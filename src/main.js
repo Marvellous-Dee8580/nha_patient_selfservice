c1304b7935121acb661c7f07c5a41c302cde7a08
Dmarveltechnologies <125516598+Marvellous-Dee8580@users.noreply.github.com>
2024-11-01 05:08:18 +0100

    commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.js'; // Change to .js extension if App is a JS file
+import App from './App.jsx'; // Change to .js extension if App is a JS file
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
